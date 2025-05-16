f585f466ab8630edb3afcb4c66432643323cb43d
TheGentleDucky <owenscruzsean@gmail.com>
2025-05-16 17:41:32 +0200

    Modified: SignUp and SignIn pages

--- JobTray-Ionic/src/app/company-pages/sign-in-company/sign-in-company.component.ts
+++ JobTray-Ionic/src/app/company-pages/sign-in-company/sign-in-company.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {CompanySignInComponent} from "../../company-components/company-sign-in/company-sign-in.component";
+import {FooterComponent} from "../../company-components/footer/footer.component";
 
 @Component({
   selector: 'app-sign-in-company',
   templateUrl: './sign-in-company.component.html',
   styleUrls: ['./sign-in-company.component.scss'],
+  imports: [
+    CompanySignInComponent,
+    FooterComponent
+  ]
 })
 export class SignInCompanyComponent  implements OnInit {
 
--- JobTray-Ionic/src/app/company-pages/sign-up-company/sign-up-company.component.ts
+++ JobTray-Ionic/src/app/company-pages/sign-up-company/sign-up-company.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {CompanySignUpComponent} from "../../company-components/company-sign-up/company-sign-up.component";
+import {FooterComponent} from "../../company-components/footer/footer.component";
 
 @Component({
   selector: 'app-sign-up-company',
   templateUrl: './sign-up-company.component.html',
   styleUrls: ['./sign-up-company.component.scss'],
+  imports: [
+    CompanySignUpComponent,
+    FooterComponent
+  ]
 })
 export class SignUpCompanyComponent  implements OnInit {
 
